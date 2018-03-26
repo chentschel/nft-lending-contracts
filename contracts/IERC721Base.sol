828f8ef9bbbae35ceb2013b96084b93b41af81f3
Christian Hentschel <chentschel@gmail.com>
2018-03-26 01:38:03 -0300

    fix: config

--- contracts/IERC721Base.sol
+++ contracts/IERC721Base.sol
@@ -1,12 +1,12 @@
-pragma solidity ^0.4.19;
+pragma solidity ^0.4.21;
 
 /**
  * @title Interface for contracts conforming to ERC-721
  */
 interface IERC721Base {
-  function ownerOf(uint256 assetId) public view returns (address);
-  function setApprovalForAll(address operator, bool authorized) public;
-  function safeTransferFrom(address from, address to, uint256 assetId) public;
-  function isAuthorized(address operator, uint256 assetId) public view returns (bool);
-  function isApprovedForAll(address operator, address assetOwner) public view returns (bool);
+  function ownerOf(uint256 assetId) external view returns (address);
+  function setApprovalForAll(address operator, bool authorized) external;
+  function safeTransferFrom(address from, address to, uint256 assetId) external;
+  function isAuthorized(address operator, uint256 assetId) external view returns (bool);
+  function isApprovedForAll(address operator, address assetOwner) external view returns (bool);
 }
